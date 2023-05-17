fdaa2affeafa09349e18f2ff3c37bc73cce528be
alessandro.vinceti <alessandro.vinceti@fht.org>
2023-05-17 14:54:59 +0200

    Fixed chronos script

---
+++ src/exec/run_Chronos.py
@@ -0,0 +1,43 @@
+import argparse
+import chronos
+import pandas as pd
+
+
+def main():
+    # create ArgParser object
+    parser = argparse.ArgumentParser(description='Run Chronos')
+
+    # add arguments
+    parser.add_argument('--lfc', dest = 'lfc', type = str, required = True,
+                        help = 'LFC dataset to correct')
+
+    parser.add_argument('--cn', dest = 'cn', type = str, required = True,
+                        help = 'Copy number dataset to use for correction')
+    
+    parser.add_argument('-o', '--out', dest = 'out', type = str, required = True,
+                        help = 'Path to save corrected file')
+    
+    args = parser.parse_args()
+
+    # Load data
+    gene_effects = pd.read_csv(args.lfc, index_col=0).T
+
+    cn = pd.read_csv(args.cn, index_col=0)
+    cn.columns = cn.columns.str.replace(r'\s\(.*\)', '', regex=True)
+
+    common_genes = list(set(gene_effects.columns) & set(cn.columns))
+    cn = cn[common_genes]
+
+    ## For genes not in CN data, assume no effect
+    for col in set(gene_effects.columns) - set(cn.columns):
+        cn = pd.concat([cn, pd.DataFrame({col: [1]*cn.shape[0]}, index=cn.index)], axis=1)
+
+    # Run Chronos
+    corrected, _ = chronos.alternate_CN(gene_effects, cn)
+
+    # Save results
+    corrected.to_csv(args.out)
+
+
+if __name__ == '__main__':
+    main()
--- src/exec/run_tools.sh
+++ src/exec/run_tools.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 source ~/.bashrc
-conda activate CN_bench_r
 
 ROOT=$1
 libs=("Avana" "KY")
@@ -13,9 +12,32 @@ then
 fi
 
 # run CCR
-for l in ${libs[@]}
-do
-	Rscript $ROOT/src/exec/run_CCR.r $ROOT/data/raw/"$l"_sgrna_raw_LFC.csv $ROOT/data/"$l"GuideMap.csv data/corrected/ "$l"
-done
+run_CCR(){
+	conda activate CN_bench_r
 
-conda deactivate
+	for l in ${libs[@]}
+	do
+		Rscript $ROOT/src/exec/run_CCR.r $ROOT/data/raw/"$l"_sgrna_raw_LFC.csv $ROOT/data/"$l"GuideMap.csv data/corrected/ "$l"
+	done
+
+	conda deactivate
+}
+
+
+
+# run Chronos
+run_Chronos(){
+	conda activate CN_bench
+
+	for l in ${libs[@]}
+	do
+		python3 $ROOT/src/exec/run_Chronos.py --lfc $ROOT/data/raw/"$l"_gene_raw_LFC.csv --cn $ROOT/data/OmicsCNGene.csv -o $ROOT/data/corrected/"$l"_gene_Chronos.csv
+	done
+
+	conda deactivate
+}
+
+
+# execute the functions
+run_CCR
+run_Chronos
