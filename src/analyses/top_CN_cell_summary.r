0b10edfbc69a4bd5df868f848c7377b631d62fa4
alessandro.vinceti <alessandro.vinceti@fht.org>
2023-06-14 21:55:09 +0200

    CN correction summary on the whole dataset

--- src/analyses/top_CN_cell_summary.r
+++ src/analyses/top_CN_cell_summary.r
@@ -16,15 +16,6 @@ CN_abs <- read_csv('data/cnv_abs_copy_number_picnic_20191101.csv') %>%
     mutate_all(as.numeric) %>%
     rownames_to_column("CellLineName") %>%
     pivot_longer(-CellLineName, names_to = "Gene", values_to = "CN_abs") %>%
-    group_by(CellLineName) %>%
-    mutate(Model_sd = sd(CN_abs)) %>%
-    ungroup() %>%
-    arrange(desc(Model_sd)) %>%
-    ## select top 25 models
-    nest(data = c(Gene, CN_abs)) %>% 
-    slice(1:25) %>%
-    unnest(data) %>%
-    select(-Model_sd) %>%
     ## convert cell line names to model IDs
     inner_join(read_csv('data/Model.csv') %>%
         select(ModelID, CellLineName)) %>%
@@ -65,28 +56,19 @@ for (lib in libs){
 
 
     ## save results
-    models <- dfs %>%
-        pull(ModelID) %>%
-        unique()
-    
-    for (model in models){
-        p_cn_abs <- dfs %>% 
-            filter(ModelID == model) %>%
-            ggplot(aes(x = as.factor(CN_abs), y = LFC, fill = Algorithm)) +
-            geom_boxplot(outlier.shape = NA) +
-            labs(x = "Absolute CN", y = "LFC", title = "CN correction across top varying cells") +
-            theme_bw() +
-            theme(
-                panel.grid.major = element_blank(),
-                panel.grid.minor = element_blank(),
-                axis.text = element_text(size = 10, color = 'black'),
-                axis.title = element_text(size = 12),
-                plot.title = element_text(size = 14, hjust = 0.5),
-                axis.text.x = element_text(hjust = 1),
-                aspect.ratio = 0.5) +
-            geom_hline(yintercept = 0, linetype = "dashed", color = "black")
-        ggsave(p_cn_abs, filename = paste0("results/analyses/cn_correction/", lib, "_", model, "_cn_abs.pdf"), width = 20, height = 10, units = "in", dpi = 300)
-    }
-
+    p_cn_abs <- ggplot(dfs, aes(x = as.factor(CN_abs), y = LFC, fill = Algorithm)) +
+        geom_boxplot(outlier.shape = NA) +
+        labs(x = "Absolute CN", y = "LFC", title = paste0("CN correction on ", lib, " dataset")) +
+        theme_bw() +
+        theme(
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            axis.text = element_text(size = 10, color = 'black'),
+            axis.title = element_text(size = 12),
+            plot.title = element_text(size = 14, hjust = 0.5),
+            axis.text.x = element_text(hjust = 1),
+            aspect.ratio = 0.5) +
+        geom_hline(yintercept = 0, linetype = "dashed", color = "black")
+    ggsave(p_cn_abs, filename = paste0("results/analyses/cn_correction/", lib, "_cn_abs.pdf"), width = 20, height = 10, units = "in", dpi = 300)
     saveRDS(dfs, paste0("results/analyses/cn_correction/", lib, "_cn_abs.rds"))
 }
