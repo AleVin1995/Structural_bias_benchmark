05c0191a7b5d24047a660c66f7432f9eb944350f
alessandro.vinceti <alessandro.vinceti@fht.org>
2023-06-22 21:48:30 +0200

    edit name

--- src/figures/sig_biomarkers_viz.r
+++ src/figures/sig_biomarkers_viz.r
@@ -4,9 +4,11 @@ libs <- c("Avana", "KY")
 
 # iterate over algorithms and libraries
 for (lib in libs){
-    biomarkers_lib_ssd <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_sig_biomarkers_ssd.rds"))
+    sig_biomarkers_ssd <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_sig_biomarkers_ssd.rds"))
+    sig_biomarkers_onco <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_sig_biomarkers_onco.rds"))
 
-    p_sig_biomark_ssd <- ggplot(biomarkers_lib_ssd, aes(x = Algorithm, y = n_sig_biomark, fill = Algorithm)) +
+    ## save results
+    p_sig_biomark_ssd <- ggplot(sig_biomarkers_ssd, aes(x = Algorithm, y = n_sig_biomark, fill = Algorithm)) +
         geom_bar(stat = "identity") +
         labs(x = "Method", y = "Nº significant associations (Strong Selective Dependencies)", 
             title = "Nº significant associations") +
@@ -21,9 +23,7 @@ for (lib in libs){
             aspect.ratio = 1)
     ggsave(p_sig_biomark_ssd, filename = paste0("results/analyses/impact_data_quality/", lib, "_sig_biomarkers_ssd.pdf"), width = 10, height = 10)
 
-    biomarkers_lib_onco <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_sig_biomarkers_onco.rds"))
-
-    p_sig_biomark_onco <- ggplot(biomarkers_lib_onco, aes(x = Algorithm, y = n_sig_biomark, fill = Algorithm)) +
+    p_sig_biomark_onco <- ggplot(sig_biomarkers_onco, aes(x = Algorithm, y = n_sig_biomark, fill = Algorithm)) +
         geom_bar(stat = "identity") +
         labs(x = "Method", y = "Nº significant associations (gain-of-function CFEs)", 
             title = "Nº significant associations") +
