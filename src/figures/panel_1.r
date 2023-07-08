01d65da3fc03ef6639d1a851977908f7f4aca1b9
alessandro.vinceti <alessandro.vinceti@fht.org>
2023-07-08 17:49:42 +0200

    color palette for panel 2

--- src/figures/panel_1.r
+++ src/figures/panel_1.r
@@ -9,6 +9,7 @@ font_import(paths = "/group/iorio/Alessandro/CN_benchmark/arial", prompt = FALSE
 source("src/figures/utils.r")
 
 libs <- c("Avana", "KY")
+cols <- c("#B3B3B3", brewer.pal(n = 7, name = "Paired"))
 
 # iterate over algorithms and libraries
 for (lib in libs){
@@ -33,7 +34,8 @@ for (lib in libs){
             text = element_text(family = "Arial"),
             plot.margin = grid::unit(c(5,5,5,5), "mm")) +
         geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
-        facet_wrap(~Algorithm, scales = "free", ncol = 4)
+        facet_wrap(~Algorithm, scales = "free", ncol = 4) +
+        scale_fill_manual(values = cols)
 
     ## LFC vs CN effect size (all genes)
     dfs_es <- dfs %>%
@@ -61,7 +63,8 @@ for (lib in libs){
             axis.text.x = element_text(hjust = 1, angle = 45, vjust = 1),
             text = element_text(family = "Arial"),
             plot.margin = grid::unit(c(5,5,5,5), "mm")) +
-        geom_hline(yintercept = 0, linetype = "dashed", color = "black")
+        geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+        scale_color_manual(values = cols)
 
 
     ## Consider only unexpressed genes (TPM < 1)
@@ -84,7 +87,8 @@ for (lib in libs){
             text = element_text(family = "Arial"),
             plot.margin = grid::unit(c(5,5,5,5), "mm")) +
         geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
-        facet_wrap(~Algorithm, scales = "free", ncol = 4)
+        facet_wrap(~Algorithm, scales = "free", ncol = 4) +
+        scale_fill_manual(values = cols)
 
     ## LFC vs CN effect size (unexpressed genes)
     dfs_es_unexpr <- dfs_unexpr %>%
@@ -112,7 +116,8 @@ for (lib in libs){
             axis.text.x = element_text(hjust = 1, angle = 45, vjust = 1),
             text = element_text(family = "Arial"),
             plot.margin = grid::unit(c(5,5,5,5), "mm")) +
-        geom_hline(yintercept = 0, linetype = "dashed", color = "black")
+        geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+        scale_fill_manual(values = cols)
     
 
     # Create panel
@@ -124,16 +129,19 @@ for (lib in libs){
             legend.title = element_text(size=24),
             legend.text = element_text(size=22)))
 
-    panel1 <- plot_grid(p_cn_abs, 
+    panel_all <- plot_grid(p_cn_abs, 
         p_es + theme(legend.position="none"), 
         nrow = 1, ncol = 2, align = 'h', axis = 'bt', 
         rel_widths = c(2, 1), rel_heights = c(1, 1),
         labels = "AUTO", label_size = 40, family = "Arial")
-    panel1 <- plot_grid(panel1, legend, rel_widths = c(4, .5))
-    ggsave(panel1, filename = paste0("results/panels/panel_cn_bias_all_", lib, ".pdf"), width = 45, height = 15, units = "in", dpi = 300)
+    panel_all <- plot_grid(panel_all, legend, rel_widths = c(4, .5))
+    ggsave(panel_all, filename = paste0("results/panels/panel_cn_bias_all_", lib, ".pdf"), width = 45, height = 15, units = "in", dpi = 300)
     
-    plot_grid(p_cn_abs_unexpr, p_es_unexpr, nrow = 1, ncol = 2,
-        align = 'h', axis = 'bt', rel_widths = c(2, 1, 2, 1),
-        labels = "AUTO", size = 40, family = "Arial") +
-        theme(text = element_text(family = "Arial"))
+    panel_unexpr <- plot_grid(p_cn_abs_unexpr, 
+        p_es_unexpr + theme(legend.position="none"), 
+        nrow = 1, ncol = 2, align = 'h', axis = 'bt', 
+        rel_widths = c(2, 1), rel_heights = c(1, 1),
+        labels = "AUTO", label_size = 40, family = "Arial")
+    panel_unexpr <- plot_grid(panel_unexpr, legend, rel_widths = c(4, .5))
+    ggsave(panel_unexpr, filename = paste0("results/panels/panel_cn_bias_unexpr_", lib, ".pdf"), width = 45, height = 15, units = "in", dpi = 300)
 }
