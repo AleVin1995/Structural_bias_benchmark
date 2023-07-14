2a2a516b782774b3eb2e1c796832231b67615784
alessandro.vinceti <alessandro.vinceti@fht.org>
2023-07-14 17:13:49 +0200

    Bug fixing panel 3

--- src/figures/panel_3.r
+++ src/figures/panel_3.r
@@ -45,7 +45,7 @@ for (lib in libs){
         theme_bw() +
         theme(
             axis.text = element_text(size = 25, color = 'black'),
-            axis.text.x = element_blank(),
+            axis.text.x = element_text(hjust = 1, angle = 45, vjust = 1),
             axis.title = element_text(size = 30, color = 'black'),
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
@@ -89,17 +89,17 @@ for (lib in libs){
         theme_bw() +
         theme(
             axis.text = element_text(size = 25, color = 'black'),
-            axis.text.x = element_blank(),
+            axis.text.x = element_text(hjust = 1, angle = 45, vjust = 1),
             axis.title = element_text(size = 30, color = 'black'),
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
             aspect.ratio = 1,
             text = element_text(family = "Arial"),
             plot.margin = unit(c(2, 2, 2, 2), "cm"),
-            legend.key.size = unit(1.5, 'cm'),
-            legend.text = element_text(size = 14),
+            legend.key.size = unit(1, 'cm'),
+            legend.text = element_text(size = 12),
             legend.position = c(0.15, 0.9),
-            legend.background = element_rect(fill = "white", color = "black")) +
+            legend.background = element_rect(fill = "white", color = NA)) +
         scale_fill_manual(labels = c("TP53 mut", "TP53 wt"), 
             values = c("#1F78B4", "#A6CEE3"), name = "")
     
@@ -144,65 +144,17 @@ for (lib in libs){
             aspect.ratio = 1,
             text = element_text(family = "Arial"),
             plot.margin = unit(c(2, 2, 2, 2), "cm"),
-            legend.key.size = unit(1.5, 'cm'),
-            legend.text = element_text(size = 14),
+            legend.key.size = unit(1, 'cm'),
+            legend.text = element_text(size = 12),
             legend.position = c(0.15, 0.9),
-            legend.background = element_rect(fill = "white", color = "black")) +
+            legend.background = element_rect(fill = "white", color = NA)) +
         scale_fill_manual(labels = c("CDKN2A mut", "CDKN2A wt"), 
             values = c("#33A02C", "#B2DF8A"), name = "")
-        
-    ## CDKN2B results
-    bm_CDKN2B <- readRDS(paste0("results/analyses/proximity_bias/", lib, "_bm_CDKN2B.rds"))
-    bm_CDKN2B$Algorithm <- factor(bm_CDKN2B$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
-    bm_CDKN2B$Coord <- factor(bm_CDKN2B$Coord, levels = c(paste0(rep(c(1:23, "X", "Y"), each = 2), c("p", "q"))))
-
-    p_CDKN2B <- ggplot(bm_CDKN2B, aes(x = Algorithm, y = est, fill = Status)) +
-        geom_bar(stat = "identity", position=position_dodge()) +
-        geom_hline(yintercept = 0.5, linetype = "dashed") +
-        labs(x = "", y = "P(intra-arm cosine > inter)") +
-        theme_bw() +
-        theme(
-            strip.background = element_blank(), 
-            strip.placement = "outside",
-            strip.text = element_text(size = 12, color = 'black'),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.text = element_text(size = 10, color = 'black'),
-            axis.title = element_text(size = 12),
-            plot.title = element_text(size = 14, hjust = 0.5),
-            axis.text.x = element_text(angle = 45, hjust = 1),
-            aspect.ratio = 1,
-            panel.spacing.y = unit(4, "lines")) +
-        facet_wrap(~Coord) +
-        scale_fill_manual(labels = c("CDKN2B mut", "CDKN2B wt"), 
-            values = c("#FF7F00", "#FDBF6F"), name = "")
-    ggsave(p_CDKN2B, filename = paste0("results/panels/proximity_bias/proximity_bias_", lib, "_bm_CDKN2B.pdf"), width = 15, height = 15, dpi = 300)
-
-    p_CDKN2B_sum <- ggplot(bm_CDKN2B, aes(x = Algorithm, y = est, fill = Status)) +
-        geom_boxplot() +
-        geom_hline(yintercept = 0.5, linetype = "dashed") +
-        labs(x = "", y = "") +
-        theme_bw() +
-        theme(
-            axis.text = element_text(size = 25, color = 'black'),
-            axis.text.x = element_text(hjust = 1, angle = 45, vjust = 1),
-            axis.title = element_text(size = 30, color = 'black'),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            aspect.ratio = 1,
-            text = element_text(family = "Arial"),
-            plot.margin = unit(c(2, 2, 2, 2), "cm"),
-            legend.key.size = unit(1.5, 'cm'),
-            legend.text = element_text(size = 14),
-            legend.position = c(0.15, 0.9),
-            legend.background = element_rect(fill = "white", color = "black")) +
-        scale_fill_manual(labels = c("CDKN2B mut", "CDKN2B wt"), 
-            values = c("#FF7F00", "#FDBF6F"), name = "")
     
     ## Assemble panel
-    panel <- p_pool_sum + p_TP53_sum + p_CDKN2A_sum + p_CDKN2B_sum +
+    panel <- p_pool_sum + p_TP53_sum + p_CDKN2A_sum +
         plot_annotation(tag_levels = 'A') &
         theme(plot.tag.position = c(0, 1.05),
             plot.tag = element_text(size = 40, face = "bold", family = "Arial"))
-    ggsave(panel, filename = paste0("results/panels/proximity_bias/proximity_bias_", lib, "_bm_all.pdf"), width = 25, height = 25, dpi = 300)
+    ggsave(panel, filename = paste0("results/panels/proximity_bias/proximity_bias_", lib, "_bm_all.pdf"), width = 30, height = 15, dpi = 300)
 }
