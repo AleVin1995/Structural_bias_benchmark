b6b3708ac686d7fb75bf2c91271641a3c85594a0
alessandro.vinceti <alessandro.vinceti@fht.org>
2023-09-09 13:38:23 +0200

    Conclusive panel
    Changed metric to assess CN bias

--- src/figures/conclusive_panel.r
+++ src/figures/conclusive_panel.r
@@ -20,37 +20,7 @@ libs <- c("Avana", "KY")
 cols <- brewer.pal(n = 7, name = "Dark2")
 
 # iterate over algorithms and libraries
-for (lib in libs){
-    ## All genes
-    dfs <- readRDS(paste0("results/analyses/cn_correction/", lib, "_cn_abs.rds"))
-    dfs$Algorithm <- factor(dfs$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
-
-    ## LFC vs CN effect size (all genes)
-    dfs_es <- dfs %>%
-        group_by(Algorithm, CN_abs) %>%
-        mutate(es = mean(LFC)/sd(LFC)) %>%
-        ungroup() %>%
-        select(Algorithm, es) %>%
-        group_by(Algorithm) %>%
-        summarise(es = median(abs(es))) %>%
-        ungroup() %>%
-        dplyr::rename(cn_bias_all = es)
-
-    ## Consider only unexpressed genes (TPM < 1)
-    dfs_unexpr <- readRDS(paste0("results/analyses/cn_correction/", lib, "_cn_abs_tpm.rds"))
-    dfs_unexpr$Algorithm <- factor(dfs_unexpr$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
-    
-    ## LFC vs CN effect size (unexpressed genes)
-    dfs_es_unexpr <- dfs_unexpr %>%
-        group_by(Algorithm, CN_abs) %>%
-        mutate(es = mean(LFC)/sd(LFC)) %>%
-        ungroup() %>%
-        select(Algorithm, es) %>%
-        group_by(Algorithm) %>%
-        summarise(es = median(abs(es))) %>%
-        ungroup() %>%
-        dplyr::rename(cn_bias_unexpr = es)
-    
+for (lib in libs){    
     ## pooled results
     bm_pool <- readRDS(paste0("results/analyses/proximity_bias/", lib, "_bm_pool.rds"))
     bm_pool$Algorithm <- factor(bm_pool$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
@@ -60,27 +30,46 @@ for (lib in libs){
         ungroup() %>%
         dplyr::rename(proximity_bias = est)
     
+    ## Recall curve amplified genes
+    rec_ampl <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_recall_ampl.rds"))
+    rec_ampl$Algorithm <- factor(rec_ampl$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
+    rec_ampl <- rec_ampl %>%
+        group_by(Algorithm) %>%
+        summarise(Recall = median(Recall)) %>%
+        ungroup() %>%
+        dplyr::rename(Recall_ampl = Recall)
+    
+    ## Recall curve amplified genes (unexpressed)
+    rec_ampl_noexpr <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_recall_ampl_noexpr.rds"))
+    rec_ampl_noexpr$Algorithm <- factor(rec_ampl_noexpr$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
+    rec_ampl_noexpr <- rec_ampl_noexpr %>%
+        group_by(Algorithm) %>%
+        summarise(Recall = median(Recall)) %>%
+        ungroup() %>%
+        dplyr::rename(Recall_ampl_noexpr = Recall)
+    
     ## merge results
-    res <- dfs_es %>%
-        left_join(dfs_es_unexpr) %>%
+    res <- rec_ampl %>%
+        left_join(rec_ampl_noexpr) %>%
         left_join(bm_pool) %>%
         filter(Algorithm != "Uncorrected") %>%
         mutate(Type = c("Unsupervised", "Supervised", "Supervised", 
             "Supervised", "Unsupervised", "Unsupervised", "Supervised")) %>%
         mutate(MoA = c("Single-screen", "Multi-screen", "Single-screen", 
             "Single-screen", "Single-screen", "Single-screen", "Multi-screen"))
+    colnames(res)[2:3] <- c("cn_bias_all", "cn_bias_unexpr")
     
     ## plot
-    radius_1 <- circleFun(c(0,0.5), 0.1, 0.05)
-    radius_2 <- circleFun(c(0,0.5), 0.2, 0.1)
-    radius_3 <- circleFun(c(0,0.5), 0.3, 0.15)
-    radius_4 <- circleFun(c(0,0.5), 0.4, 0.2)
-    radius_5 <- circleFun(c(0,0.5), 0.5, 0.25)
-    radius_6 <- circleFun(c(0,0.5), 0.6, 0.3)
-    radius_7 <- circleFun(c(0,0.5), 0.7, 0.35)
-    radius_8 <- circleFun(c(0,0.5), 0.8, 0.4)
-    radius_9 <- circleFun(c(0,0.5), 0.9, 0.45)
-    radius_10 <- circleFun(c(0,0.5), 1, 0.5)
+    radius_1 <- circleFun(c(0.5,0.5), 0.1, 0.05)
+    radius_2 <- circleFun(c(0.5,0.5), 0.2, 0.1)
+    radius_3 <- circleFun(c(0.5,0.5), 0.3, 0.15)
+    radius_4 <- circleFun(c(0.5,0.5), 0.4, 0.2)
+    radius_5 <- circleFun(c(0.5,0.5), 0.5, 0.25)
+    radius_6 <- circleFun(c(0.5,0.5), 0.6, 0.3)
+    radius_7 <- circleFun(c(0.5,0.5), 0.7, 0.35)
+    radius_8 <- circleFun(c(0.5,0.5), 0.8, 0.4)
+    radius_9 <- circleFun(c(0.5,0.5), 0.9, 0.45)
+    radius_10 <- circleFun(c(0.5,0.5), 1, 0.5)
 
     ### all genes
     p_es <- ggplot(res, aes(x = cn_bias_all, y = proximity_bias)) +
@@ -92,6 +81,7 @@ for (lib in libs){
             nudge_x = 0, nudge_y = 0.012, 
             size = 8,
             check_overlap = FALSE) +
+        geom_vline(xintercept = 0.5, linetype = 'longdash') +
         geom_path(data = radius_1, aes(x = x, y = y), linetype = "dashed", color = alpha("black", 0.5)) +
         geom_path(data = radius_2, aes(x = x, y = y), linetype = "dashed", color = alpha("black", 0.5)) +
         geom_path(data = radius_3, aes(x = x, y = y), linetype = "dashed", color = alpha("black", 0.5)) +
@@ -101,7 +91,7 @@ for (lib in libs){
         labs(x = "Median correction \nfor CN bias (all genes)", 
             y = "",
             title = "All genes") +
-        xlim(c(0, 0.35)) +
+        xlim(c(0.4, 0.75)) +
         ylim(c(0.5, 0.75)) +
         theme_bw() +
         theme(
@@ -128,6 +118,7 @@ for (lib in libs){
             nudge_x = 0, nudge_y = 0.012, 
             size = 9,
             check_overlap = FALSE) +
+        geom_vline(xintercept = 0.5, linetype = 'longdash') +
         geom_path(data = radius_1, aes(x = x, y = y), linetype = "dashed", color = alpha("black", 0.5)) +
         geom_path(data = radius_2, aes(x = x, y = y), linetype = "dashed", color = alpha("black", 0.5)) +
         geom_path(data = radius_3, aes(x = x, y = y), linetype = "dashed", color = alpha("black", 0.5)) +
@@ -141,7 +132,7 @@ for (lib in libs){
         labs(x = "Median correction \nfor CN bias (unexpressed genes)", 
             y = "Median correction \nfor proximity bias",
             title = "Unexpressed genes") +
-        xlim(c(0, 0.9)) +
+        xlim(c(0.35, 0.65)) +
         ylim(c(0.5, 0.75)) +
         theme_bw() +
         theme(
@@ -182,24 +173,6 @@ for (lib in libs){
     onco_auc <- onco_auc %>%
         dplyr::rename(AUROC_onco = AUROC)
 
-    ## Recall curve amplified genes
-    rec_ampl <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_recall_ampl.rds"))
-    rec_ampl$Algorithm <- factor(rec_ampl$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
-    rec_ampl <- rec_ampl %>%
-        group_by(Algorithm) %>%
-        summarise(Recall = median(Recall)) %>%
-        ungroup() %>%
-        dplyr::rename(Recall_ampl = Recall)
-    
-    ## Recall curve amplified genes (unexpressed)
-    rec_ampl_noexpr <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_recall_ampl_noexpr.rds"))
-    rec_ampl_noexpr$Algorithm <- factor(rec_ampl_noexpr$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
-    rec_ampl_noexpr <- rec_ampl_noexpr %>%
-        group_by(Algorithm) %>%
-        summarise(Recall = median(Recall)) %>%
-        ungroup() %>%
-        dplyr::rename(Recall_ampl_noexpr = Recall)
-
     ## Nº significant biomarkers (all CFEs on strongly selective dependencies)
     sig_biomarkers_ssd <- readRDS(paste0("results/analyses/impact_data_quality/", lib, "_sig_biomarkers_ssd.rds"))
     sig_biomarkers_ssd$Algorithm <- factor(sig_biomarkers_ssd$Algorithm, levels = c("Uncorrected", "CCR", "Chronos", "Crispy", "GAM", "Geometric", "LDO", "MAGeCK"))
@@ -215,14 +188,10 @@ for (lib in libs){
     ## merge results
     params <- auprcs %>%
         left_join(onco_auc) %>%
-        left_join(rec_ampl) %>%
-        left_join(rec_ampl_noexpr) %>%
         left_join(sig_biomarkers_ssd) %>%
         left_join(sig_biomarkers_onco) %>%
         left_join(gene_sep) %>%
-        mutate(Recall_ampl = 1 - abs(0.5 - Recall_ampl)*2,
-            Recall_ampl_noexpr = 1 - abs(0.5 - Recall_ampl_noexpr)*2,
-            n_sig_biomark_ssd = n_sig_biomark_ssd/max(n_sig_biomark_ssd),
+        mutate(n_sig_biomark_ssd = n_sig_biomark_ssd/max(n_sig_biomark_ssd),
             n_sig_biomark_onco = n_sig_biomark_onco/max(n_sig_biomark_onco),
             NNMD = NNMD/min(NNMD)) %>%
         filter(Algorithm != "Uncorrected")
@@ -241,8 +210,7 @@ for (lib in libs){
         legend.position = "right",
         font.radar = "Arial",
         axis.labels = c("AUPRC common\n essential genes", 
-            "AUROC\noncogenes", "Recall\n(amplified\ngenes)", 
-            "Recall (amplified\nunexpressed genes)", 
+            "AUROC\noncogenes", 
             "Nº significant\nbiomarkers\n(all CFEs)", 
             "Nº significant\nbiomarkers\n(GOF CFEs)",
             "NNMD"),
